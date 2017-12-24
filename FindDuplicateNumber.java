68f3fa261ab2c2403e016c668ebe1e23ecf4df36
Moksh Jawa <mokshjawa@gmail.com>
2017-12-24 08:56:03 -0800

    LeetCode 287. Find the Duplicate Number

---
+++ FindDuplicateNumber.java
@@ -0,0 +1,30 @@
+/*
+
+Given an array nums containing n + 1 integers where each integer is between 1 and n (inclusive), prove that at least one duplicate number must exist. Assume that there is only one duplicate number, find the duplicate one.
+
+Note:
+You must not modify the array (assume the array is read only).
+You must use only constant, O(1) extra space.
+Your runtime complexity should be less than O(n2).
+There is only one duplicate number in the array, but it could be repeated more than once.
+*/
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> visited = new HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (visited.contains(nums[i]))
+            {
+                return nums[i];
+            }
+            else
+            {
+                visited.add(nums[i]);
+            }
+        }
+        
+        return -1;
+    }
+}
