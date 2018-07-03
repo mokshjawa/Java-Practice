a867a269c5494a342f09efd872b9baa36732da5b
Moksh Jawa <mokshjawa@gmail.com>
2018-07-03 14:49:01 -0500

    LeetCode 98. Validate Binary Search Tree

---
+++ ValidBinarySearchTreeInorder.java
@@ -0,0 +1,71 @@
+/*
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+Example 1:
+
+Input:
+    2
+   / \
+  1   3
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+Output: false
+Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+             is 5 but its right child's value is 4.
+*/
+
+/**
+* Definition for a binary tree node.
+* public class TreeNode {
+*     int val;
+*     TreeNode left;
+*     TreeNode right;
+*     TreeNode(int x) { val = x; }
+* }
+*/
+class Solution {
+    public boolean isValidBST(TreeNode root)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+       
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode prev = null;
+       
+        while (root != null || !stack.isEmpty())
+        {
+            if (root != null)
+            {
+                stack.push(root);
+                root = root.left;
+            }
+            else
+            {
+                root = stack.pop();
+               
+                if (prev != null && prev.val >= root.val)
+                {
+                    return false;
+                }
+               
+                prev = root;
+                root = root.right;
+            }
+        }
+       
+        return true;
+    }
+}
---
+++ ValidBinarySearchTreeRecursive.java
@@ -0,0 +1,57 @@
+/*
+Given a binary tree, determine if it is a valid binary search tree (BST).
+
+Assume a BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+Example 1:
+
+Input:
+    2
+   / \
+  1   3
+Output: true
+Example 2:
+
+    5
+   / \
+  1   4
+     / \
+    3   6
+Output: false
+Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+             is 5 but its right child's value is 4.
+*/
+
+/**
+* Definition for a binary tree node.
+* public class TreeNode {
+*     int val;
+*     TreeNode left;
+*     TreeNode right;
+*     TreeNode(int x) { val = x; }
+* }
+*/
+class Solution {
+    public boolean isValidBST(TreeNode root)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+ 
+        return helper(root.left, Long.MIN_VALUE, root.val) && helper(root.right, root.val, Long.MAX_VALUE);  
+    }
+   
+    public boolean helper(TreeNode root, long min, long max)
+    {
+        if (root != null)
+        {
+            return (root.val > min && root.val < max) && helper(root.left, min, root.val) && helper(root.right, root.val, max);
+        }
+        
+         return true;
+    }
+}
