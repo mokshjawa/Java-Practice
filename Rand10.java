f3ff7907e41852e48c92ba7f1815e60c323723d7
Moksh Jawa <mokshjawa@gmail.com>
2018-07-16 15:03:28 -0500

    LeetCode 872. Implement Rand10() Using Rand7()

---
+++ Rand10.java
@@ -0,0 +1,55 @@
+/*
+Given a function rand7 which generates a uniform random integer in the range 1 to 7, write a function rand10 which generates a uniform random integer in the range 1 to 10.
+
+Do NOT use system's Math.random().
+
+ 
+
+Example 1:
+
+Input: 1
+Output: [7]
+Example 2:
+
+Input: 2
+Output: [8,4]
+Example 3:
+
+Input: 3
+Output: [8,1,10]
+ 
+
+Note:
+
+rand7 is predefined.
+Each testcase has one argument: n, the number of times that rand10 is called.
+ 
+
+Follow up:
+
+What is the expected value for the number of calls to rand7() function?
+Could you minimize the number of calls to rand7()?
+*/
+
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+class Solution extends SolBase {
+    public int rand10() 
+    {
+        int tgt;
+        
+        while (true)
+        {
+            tgt = 7 * rand7() + rand7() - 8;
+            
+            if (tgt < 40)
+            {
+                return tgt % 10 + 1;
+            }
+        }
+        
+    }
+}
