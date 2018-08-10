fd92b884ebf0a94699ba0127c98879f169fbcc57
Moksh Jawa <mokshjawa@gmail.com>
2018-08-10 23:21:29 +0100

    LeetCode 450. Delete Node in a BST

---
+++ DeleteNodeBST.java
@@ -0,0 +1,102 @@
+/*
+Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+
+Basically, the deletion can be divided into two stages:
+
+Search for a node to remove.
+If the node is found, delete the node.
+Note: Time complexity should be O(height of tree).
+
+Example:
+
+root = [5,3,6,2,4,null,7]
+key = 3
+
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Given key to delete is 3. So we find the node with value 3 and delete it.
+
+One valid answer is [5,4,6,2,null,null,7], shown in the following BST.
+
+    5
+   / \
+  4   6
+ /     \
+2       7
+
+Another valid answer is [5,2,6,null,4,null,7].
+
+    5
+   / \
+  2   6
+   \   \
+    4   7
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) 
+    {
+        if (root == null)
+        {
+            return root;
+        }
+        
+        if (root.val == key)
+        {
+            if (root.left == null || root.right == null)
+            {
+                if (root.left == null && root.right == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    if (root.left == null)
+                    {
+                        return root.right;
+                    }
+                    else
+                    {
+                        return root.left;
+                    }
+                }
+            }
+            else
+            {
+                TreeNode replacement = root;
+                replacement = replacement.right;
+                
+                while (replacement.left != null)
+                {
+                    replacement = replacement.left;
+                }
+                
+                root.val = replacement.val;
+                root.right = deleteNode(root.right, root.val);
+            }
+        }
+        else if (root.val > key)
+        {
+            root.left = deleteNode(root.left, key);
+        }
+        else
+        {
+            root.right = deleteNode(root.right, key);
+        }
+        
+        return root;
+    }
+}
