1828040e47f49c8c4152f97e882f10e38de4aff8
Moksh Jawa <mokshjawa@gmail.com>
2019-08-23 10:30:13 -0700

    LeetCode 96. Unique Binary Search Trees

---
+++ UniqueBinarySearchTrees.java
@@ -0,0 +1,33 @@
+/*
+Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+
+Example:
+
+Input: 3
+Output: 5
+Explanation:
+Given n = 3, there are a total of 5 unique BST's:
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        
+        return dp[n];
+    }
+}
